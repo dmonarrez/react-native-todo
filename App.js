d1435a22062fbac97bd7244fb258f4246e4d44c3
David Monarrez <davidmonarrez3@gmail.com>
2019-05-10 13:33:45 -0700

    create headder

--- App.js
+++ App.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { LinearGradient } from 'expo';
+import { primaryGradientArray } from './app/utils/Colors';
 
-import Main from './app/Main';
-
-export default class App extends React.Component {
+export default class Main extends React.Component {
 	render() {
-		return <Main />;
+		return(
+			<LinearGradient colors={primaryGradientArray} style={styles.container}>
+				<StatusBar barStyle='light-content' />
+				<Text>Open up App.js to start working on your app!</Text>
+			</LinearGradient>
+		);
 	}
 }
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1
+	}
+});
