3df923cf566347c126c7e4fdd62d3eccc15111b9
David Monarrez <davidmonarrez3@gmail.com>
2019-05-10 15:15:05 -0700

    add list component to main

--- app/Main.js
+++ app/Main.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, ScrollView } from 'react-native';
 import { LinearGradient } from 'expo';
 import { primaryGradientArray } from './utils/Colors';
 import Header from './components/Header'
@@ -22,13 +22,31 @@ export default class Main extends React.Component {
     const { inputValue } = this.state;
 		return(
 			<LinearGradient colors={primaryGradientArray} style={styles.container}>
+        //status bar
 				<StatusBar barStyle='light-content' />
+        //header
 				<View style={styles.centered}>
 					<Header title={headerTitle} />
 				</View>
+        //input
         <View style={styles.inputContainer}>
           <Input inputValue={inputValue} onChangeText={this.newInputValue} />
         </View>
+        //list
+        <View style={styles.list}>
+          <ScrollView contentContainerStyle={styles.scrollableList}>
+          {Object.values(allItems).reverse().map(item => (
+            <List 
+              key={item.id} 
+              {...item}
+              deleteItem={this.deleteItem}
+              completeItem={this.completeItem}
+              incompleteItem={this.incompleteItem} 
+            />
+          ))}
+          </ScrollView>
+        </View>
+        
 			</LinearGradient>
 		);
 	}
@@ -46,5 +64,16 @@ const styles = StyleSheet.create({
   inputContainer: {
     marginTop: 40,
     paddingLeft: 15
+  },
+  
+  list: {
+    flex: 1,
+    marginTop: 70,
+    marginBottom: 10,
+    paddingLeft: 15
+  },
+  
+  scrollableList: {
+    marginTop: 15
   }
 });
